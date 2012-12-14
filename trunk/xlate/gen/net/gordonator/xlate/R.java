926613ceca201aa306514e7829a4afb11c87ecda
josh@gordonclan.net <josh@gordonclan.net@c85f4edf-0d25-8a87-7c98-02fc265de0a6>
2012-12-14 06:28:44 +0000

    xlate updates. Whacked initial thoughts and went with different thoughts...

--- trunk/xlate/gen/net/gordonator/xlate/R.java
+++ trunk/xlate/gen/net/gordonator/xlate/R.java
@@ -14,9 +14,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int editText1=0x7f070001;
-        public static final int menu_settings=0x7f070002;
-        public static final int pager=0x7f070000;
+        public static final int button1=0x7f070002;
+        public static final int button2=0x7f070003;
+        public static final int button3=0x7f070004;
+        public static final int button4=0x7f070005;
+        public static final int button5=0x7f070006;
+        public static final int button6=0x7f070007;
+        public static final int button7=0x7f070008;
+        public static final int button8=0x7f070009;
+        public static final int editText1=0x7f07000a;
+        public static final int horizontalScrollView1=0x7f070001;
+        public static final int menu_settings=0x7f07000b;
+        public static final int toggleButton1=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -26,11 +35,8 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040004;
-        public static final int menu_settings=0x7f040005;
-        public static final int title_section1=0x7f040003;
-        public static final int title_section2=0x7f040002;
-        public static final int title_section3=0x7f040001;
+        public static final int hello_world=0x7f040001;
+        public static final int menu_settings=0x7f040002;
     }
     public static final class style {
         /** 
