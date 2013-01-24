3a6051141b9eee152621298f4581c2459c062cea
josh@gordonclan.net <josh@gordonclan.net@c85f4edf-0d25-8a87-7c98-02fc265de0a6>
2013-01-24 06:42:21 +0000

    Added copyright notice per GPL

--- Xlate/src/net/gordonator/xlate/MainActivity.java
+++ Xlate/src/net/gordonator/xlate/MainActivity.java
@@ -1,5 +1,26 @@
 package net.gordonator.xlate;
 
+/* 
+	xlate-android. Variosu Base converters
+ 	Copyright (C) 2013 Joshua Gordon 
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    This project is hosted on Google code. The home page and source code can be found at http://gordonator.net/xlate-android
+ */
+
+
 import java.math.BigInteger;
 
 import android.os.Bundle;
