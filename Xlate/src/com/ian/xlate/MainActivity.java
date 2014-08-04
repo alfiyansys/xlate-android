300c0096ace345fd28368d208737fd82de9ed00d
M Alfiyan S <alfiyan.syamsuddin@gmail.com>
2014-08-04 11:35:00 +0700

    add library, and many more
    using imported project layouts and icons, just modify the core :)

--- Xlate/src/com/ian/xlate/MainActivity.java
+++ Xlate/src/com/ian/xlate/MainActivity.java
@@ -2,13 +2,40 @@ package com.ian.xlate;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {	
+	Text text;
+	EditText editor;
+	int type = 1;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editor = (EditText) this.findViewById(R.id.textBox);
+        editor.addTextChangedListener(new TextWatcher(){
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
+				if(type == 1){
+					text = new Text(editor.getText().toString(),1);
+					Toast.makeText(getApplicationContext(), text.toText(), Toast.LENGTH_LONG).show();
+				}
+			}
+			@Override
+			public void afterTextChanged(Editable s) {				
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+			}        	
+        });
     }
 
     @Override
@@ -16,5 +43,19 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    /*
+     * onClick methods
+     */
+    
+    void dataText(){
+    	//text = new Text(editor.getText().toString(),1);
+    	type = 1;
+    	//editor.setText(text.)
+    }
+    
+    void dataHexa(){
+    	
     }
 }
--- Xlate/src/com/ian/xlate/Text.java
+++ Xlate/src/com/ian/xlate/Text.java
@@ -1,7 +1,16 @@
 package com.ian.xlate;
 
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+
 public class Text {
 	String text;
+	char[] textarray;
+	int[] integerData;
+	byte[] byteData;
 	
 	public Text(String something, int type){
 		/*
@@ -9,10 +18,31 @@ public class Text {
 		 * 1. text
 		 * 2. binary
 		 * 3. octal
-		 * 4. hexadecimal
-		 * 5. base32
-		 * 6. base64
-		 * 7. ASCII85
+		 * 4. decimal
+		 * 5. hexadecimal
+		 * 6. base32
+		 * 7. base64
+		 * 8. ASCII85
 		 */
+		
+		if(type == 1){
+			this.text = something;
+			this.textarray = something.toCharArray();
+			ByteBuffer bb = Charset.forName("UTF-8").encode(CharBuffer.wrap(something.toCharArray()));
+			byteData = new byte[bb.remaining()];
+			//bb.get(b);
+		}
+	}
+	
+	String toText(){
+		CharBuffer temp;
+		temp = Charset.forName("UTF-8").decode(ByteBuffer.wrap(byteData));
+		return temp.toString();
+	}
+	
+	String toHexa(){
+		String temp;
+		temp = StringEscapeUtils.escapeJava(text);
+		return temp;
 	}
 }
